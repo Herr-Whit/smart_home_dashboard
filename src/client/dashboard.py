9d27e1b620f787bd73cd8b4ccd92105a0938f572
Tony <anton.whittaker@gmail.com>
2024-04-23 11:02:19 +0200

    removed unneeded http

--- src/client/dashboard.py
+++ src/client/dashboard.py
@@ -14,36 +14,7 @@ import requests
 
 def download_latest_png():
     """Query the dashboard server for the latest png to display on the screen"""
-    # s = socket.socket()
-    # ai = socket.getaddrinfo(DASHBOARD_URL, 8000)
-    # addr = ai[0][-1]
-    # print("Address infos:", ai)
-    # print("Connect address:", addr)
-    # s.connect(addr)
-    #
-    # if True:
-    #     # MicroPython socket objects support stream (aka file) interface
-    #     # directly, but the line below is needed for CPython.
-    #     s = s.makefile("rwb", 0)
-    #     s.write(b"GET /dashboard/ HTTP/1.0\r\n\r\n")
-    #     print(s.read())
-    # else:
-    #     s.send(b"GET /dashboard HTTP/1.0\r\n\r\n")
-    #     print(s.recv(4096))
-    #
-    # s.close()
-    # client.request('GET', '/dashboard')
-    # response = client.getresponse()
-    # # response = requests.get(DASHBOARD_URL)
-    # # check for 200
-    # if response.status == 200:
-    #     file_name = f"dashboard.png"
-    #     file_path = os.path.join("sd", file_name)
-    #     with open(file_name, "wb") as f:
-    #         f.write(response.read())
-    #     return file_path
-    # else:
-    #     return None
+
     response = requests.get(f"http://{DASHBOARD_URL}:8000/dashboard/")
     if response.status_code == 200:
         file_name = f"dashboard.png"
