f7c8cee42353d93a8f7bb01c8893a6041c21301b
Tony <anton.whittaker@gmail.com>
2024-06-04 11:54:47 +0200

    fixed bmp rendering

--- src/server/helpers.py
+++ src/server/helpers.py
@@ -5,9 +5,10 @@ def png_to_bmp(png_path, bmp_path):
     img = Image.open(png_path)
     ary = np.array(img)
 
-    ary = ary.mean(axis=2)
+    if len(ary.shape) == 3:
+        ary = ary.mean(axis=2)
 
-    bitmap = np.dot((ary > 200).astype(float),255)
-    im = Image.fromarray(bitmap.astype(np.uint8))
+    # bitmap = np.dot((ary > 200).astype(float),255)
+    im = Image.fromarray(ary.astype(np.uint8))
     im.save(bmp_path)
     return bmp_path
