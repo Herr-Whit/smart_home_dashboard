03620cdaba3639fbdb7409e2982c62a119374645
Tony <anton.whittaker@gmail.com>
2024-11-01 15:34:21 +0100

    changed to run client on arduino/c

---
+++ src/client/inplate_10_dashboard/inplate_10_dashboard.ino
@@ -0,0 +1,55 @@
+#include <Inkplate.h>
+#include "secrets.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+Inkplate display(INKPLATE_3BIT);
+int count = 0;
+char str[20];
+const char* DASHBOARD_URL = "192.168.178.42";
+
+int getTimingInfo() {
+    Serial.println("Getting timing info...");
+    HTTPClient http;
+    http.begin("http://192.168.178.42:8000/timing/");
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode == 200) {
+        String payload = http.getString();
+        http.end();
+
+        // Deserialize JSON
+        DynamicJsonDocument doc(1024);
+        deserializeJson(doc, payload);
+
+        // Access "time_to_sleep" from JSON response
+        int sleep_duration = doc["time_to_sleep"].as<int>() * 1000;
+        return sleep_duration;
+
+    } else {
+        Serial.printf("Could not get timing info: %d\n", httpResponseCode);
+        http.end();
+        return 10;
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+
+  display.begin();
+  while (!display.connectWiFi(ssid, pass)) {
+    Serial.println("Connecting to wifi...");
+    delay(1000);
+  }
+  Serial.println("Connected to wifi!");
+  display.clearDisplay();
+}
+
+void loop() {
+  display.clearDisplay();
+  display.drawBitmapFromWeb("http://192.168.178.42:8000/dashboard/", 0, 0);
+  display.display();
+  int sleep_duration = getTimingInfo() * 1000;
+  delay(sleep_duration);
+}
